7c3b0f64f865d29ac271a762a5bd23c6eb30970b
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-08-20 11:59:49 -0500

    fixing linters

--- project1.rb
+++ project1.rb
@@ -1,18 +1,16 @@
 def bubble_sort(arr)
-  # a = 0
   b = arr.size - 1
-  sorted = false
-  
-  while (!sorted && b > 0) do # we continue the traitement only if the array is not sorted and b>0
+  sorted = false  
+  while !sorted && b.positive? do # we continue the traitement only if the array is not sorted and b>0
     sorted = true
     0.upto(b - 1) do |i|
-      if arr[i] > arr[i + 1]
+      next arr if arr[i] > arr[i + 1]
         # switching the order
         temp = arr[i] # temporary container of arr[i] value
-        arr[i] = arr[i+1]
+        arr[i] = arr[i + 1]
         arr[i + 1] = temp
-        sorted = false  # the arr is nom sorted
-      end
+        sorted = false # the arr is nom sorted
+        next
     end
     b -= 1 # we have the biggest element in place, we go to the next biggest element
     print arr
@@ -22,3 +20,5 @@ def bubble_sort(arr)
   print arr
   puts
 end
+
+bubble_sort([8, 6, 9, 5, 2, 4, 4, 1, 0])
