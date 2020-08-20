84afa33176a58b957df288d44b29b67b04853381
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-08-20 17:32:16 -0500

    adding bubble_sort_by

--- project1.rb
+++ project1.rb
@@ -1,24 +1,39 @@
 def bubble_sort(arr)
   b = arr.size - 1
   sorted = false
-  while !sorted && b.positive? # we continue the traitement only if the array is not sorted and b>0
+  while !sorted && b.positive?
     sorted = true
     0.upto(b - 1) do |i|
       next arr if arr[i] <= arr[i + 1]
 
-      # switching the order
-      temp = arr[i] # temporary container of arr[i] value
+      temp = arr[i]
       arr[i] = arr[i + 1]
       arr[i + 1] = temp
-      sorted = false # the arr is nom sorted
+      sorted = false
     end
-    b -= 1 # we have the biggest element in place, we go to the next biggest element
-    print arr
-    print ' ' + sorted.to_s
-    puts
+    b -= 1
   end
-  print arr
-  puts
+  return arr
 end
 
-bubble_sort([8, 6, 9, 5, 2, 4, 4, 1, 0])
+def bubble_sort_by(arr)
+  b = arr.size - 1
+  sorted = false
+  while !sorted && b.positive?
+    sorted = true
+    0.upto(b - 1) do |i|
+      if (yield arr[i], arr[i + 1]) > 0
+        temp = arr[i]
+        arr[i].length = arr[i + 1].length
+        arr[i + 1] = temp
+        sorted = false
+      end
+    end
+    b -= 1
+  end
+  return arr
+end
+
+print bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
